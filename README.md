9e44488fa89a6e77a64ea757742302665679fdfd
kentonself <kentonself@gmail.com>
2023-02-18 16:36:32 -0600

    Added rest4dict integration. Made the concatenate option clearer. Moved the  api calls to their own method for clarity (and for pylint)

--- README.md
+++ README.md
@@ -10,43 +10,47 @@ create a card in an Anki deck for future recall/study. API calls are threaded fo
 ## Requirements
 
 1. Python installed.
-2. An account with rapidapi.com  https://rapidapi.com/
-3. A subscription within rapidapi to one or more Spanish translation API's.
-   Currently 3 services are supported, all of which have a free (or "freemium") tier:
-    - MyMemory Translation Memory: https://rapidapi.com/translated/api/mymemory-translation-memory
-    - Long Translator: https://rapidapi.com/cloudlabs-dev/api/long-translator 
-    - Rapid Translate Multi Traduction: https://rapidapi.com/sibaridev/api/rapid-translate-multi-traduction
-4. A running instance of Anki
-5. A deck in Anki to add new cards to.
-6. The AnkiConnect Add-In (https://ankiweb.net/shared/info/2055492159)
+2. Either 
+    - rest4dict running on the localhost (http://github.com/kentonself/rest4dict)  AND/OR
+    - An account with rapidapi.com  https://rapidapi.com/ AND
+    - A subscription within rapidapi to one or more translation API
+        -  Currently 3 rapidapi services are supported, all of which have a free (or "freemium") tier:
+            - MyMemory Translation Memory: https://rapidapi.com/translated/api/mymemory-translation-memory
+            - Long Translator: https://rapidapi.com/cloudlabs-dev/api/long-translator
+            - Rapid Translate Multi Traduction: https://rapidapi.com/sibaridev/api/rapid-translate-multi-traduction
+3. A running instance of Anki with the AnkiConnect Add-In (https://ankiweb.net/shared/info/2055492159)
+4. A deck in Anki to add new cards to.
 
 ## Environment
 
 Spanki uses the following environment variables:
 ```
-SPANKI_KEY - Mandatory. The key from rapidapi ("X-RapidAPI-Key") for your subscriptions
-SPANKI_APIS - Mandatory A comma separated list of APIs to check
-SPANKI_DECK_NAME - Mandatory. The deck name in Anki to add cards to.
-SPANKI_NOTE_TYPE - Optional. If set Spanki will add fields for gender and part of speech, oherwise wil insert a basic front/back card
-SPANKI_SYNC_ON_ADD - Optional. If set (unless set to 0 or False), will sync with AnkiWeb after each add. 
-                       (Can add significant time for sync)
-SPANKI_LANG - Optional. Language code to be translated. Ex. "es" for Spanish, "it" for Italian, etc.
-                       (Defaults to Spanish. Only tested in Spanish, so YMMV)
+SPANKI_APIS            - Mandatory A comma separated list of APIs to check (see Examples below)
+SPANKI_KEY             - Mandatory if using rapidapi. The key from rapidapi ("X-RapidAPI-Key") for your subscriptions
+SPANKI_DECK_NAME       - Mandatory. The deck name in Anki to add cards to.
+SPANKI_NOTE_TYPE       - Optional. If set Spanki will add fields for gender and part of speech, oherwise wil insert a
+                                   basic front/back card
+SPANKI_SYNC_ON_ADD     - Optional. If set (unless set to 0 or False), will sync with AnkiWeb after each add.
+                                  (Can add time for sync)
+SPANKI_LANG            - Optional. Language code to be translated. Ex. "es" for Spanish, "it" for Italian, etc.
+                                   (Defaults to Spanish. Only tested in Spanish, so YMMV)
+SPANKI_ANKICONNECT_URL - Optional. If set will override the default AnkiConnect address http://localhost:8765
 ```
 Examples:
 ```
 export SPANKI_KEY=0123456789abcdef123456789aabcdef123456789abcdef01
-export SPANKI_APIS=mymemory,multi-traduction,long-translator
+export SPANKI_APIS=mymemory,multi-traduction,long-translator,rest4dict
 export SPANKI_DECK_NAME=Kenton\'s\ Spanish\ Words
 export SPANKI_NOTE_TYPE=Spanish
 export SPANKI_SYNC_ON_ADD=1
 export SPANKI_LANG=es
+export SPANK_ANKICONNECT_URL=http://remotehost.local:8765
 ```
 
 
 ## Usage
 
-Run spanky.py from the command line. Enter a word or phrase in Spanish. When translation is returned, spanki can create 
+Run spanky.py from the command line. Enter a word or phrase in Spanish. When translation is returned, spanki can create
 a card and add it to the Anki deck. You can enter the part of speech and gender (for nouns)
 
 ## Examples
@@ -83,7 +87,6 @@ GNU GENERAL PUBLIC LICENSE
 ## TODO
 
 - Add other target languages besides English
-- Add an anki url besides localhost. A little questionable from a security standpoint, but user can assume the risk.
 - Add more API's
 - Look up words in fd-spa-eng dictionary of linux `dict` command (incorporate with rest4dict)
 - Parse the return from API call to AnkiConnect to be cleaner
--- spanki.py
+++ spanki.py
@@ -4,81 +4,105 @@
 """
 import os
 import sys
+import re
 import threading
 import requests
 
-supported_apis = ['mymemory', 'multi-traduction','long-translator']
+supported_apis = ['mymemory', 'multi-traduction','long-translator', 'rest4dict']
+rapidapi_apis = ['mymemory', 'multi-traduction','long-translator']
+
 # pylint: disable=invalid-name
 
 def call_api(endpoint, text):
     """ Threaded function that makes API call """
     if endpoint == 'mymemory':
-        url  = 'https://translated-mymemory---translation-memory.p.rapidapi.com/get'
-        headers = {'X-RapidAPI-Key': os.environ.get('SPANKI_KEY')}
-        params =  {'langpair': lang + '|en', 'q': text}
-
-        response = requests.get(url=url, timeout = 10, headers = headers, params=params)
-        if response.status_code != 200:
-            print(f'API {endpoint} returned {response.status_code}')
-            xlations[endpoint] =  None
-        elif response.json()['responseData']['translatedText'] is None:
-            print (f'No translation for {orig}')
-            xlations[endpoint] =  None
-        else:
-            xlations[endpoint] =  response.json()['responseData']['translatedText'].lower()
-        if xlations[endpoint] == orig:
-            xlations[endpoint]=None
+        mymemory_api(endpoint, text)
     if endpoint == 'multi-traduction':
-        url = "https://rapid-translate-multi-traduction.p.rapidapi.com/t"
-
-        payload = {
-        	"from": lang,
-        	"to": "en",
-        	"e": "",
-        	"q": [text]
-        }
-        headers = {
-        	"content-type": "application/json",
-        	"X-RapidAPI-Key": os.environ.get('SPANKI_KEY'),
-        	"X-RapidAPI-Host": "rapid-translate-multi-traduction.p.rapidapi.com"
-        }
-        response = requests.post(url, json=payload, headers=headers, timeout=10)
-        if response.status_code != 200:
-            print(f'API {endpoint} returned {response.status_code}')
-            xlations[endpoint] =  None
-        else:
-            xlations[endpoint]= None if response.json()[0] == orig else response.json()[0]
-
+        multi_traduction_api(endpoint, text)
     if endpoint == 'long-translator':
-        url = "https://long-translator.p.rapidapi.com/translate"
+        long_translator_api(endpoint, text)
+    if endpoint == 'rest4dict':
+        rest4dict_api(endpoint, text)
 
-        payload = {
-                "source_language": lang,
-                "target_language": "en",
-                "text": text
-                }
-        headers = {
+def mymemory_api(endpoint, text):
+    "Call the rapidapi mymemory call"
+    url  = 'https://translated-mymemory---translation-memory.p.rapidapi.com/get'
+    headers = {'X-RapidAPI-Key': spanki_key}
+    params =  {'langpair': lang + '|en', 'q': text}
+
+    response = requests.get(url=url, timeout = 10, headers = headers, params=params)
+    if response.status_code != 200:
+        print(f'API {endpoint} returned {response.status_code}')
+        xlations[endpoint] =  None
+    elif response.json()['responseData']['translatedText'] is None:
+        print (f'No translation for {orig}')
+        xlations[endpoint] =  None
+    else:
+        xlations[endpoint] =  response.json()['responseData']['translatedText'].lower()
+    if xlations[endpoint] == orig:
+        xlations[endpoint]=None
+
+def multi_traduction_api(endpoint, text):
+    "Call the rapidapi multi-traduction call"
+    url = "https://rapid-translate-multi-traduction.p.rapidapi.com/t"
+
+    payload = {
+    	"from": lang,
+    	"to": "en",
+    	"e": "",
+    	"q": [text]
+    }
+    headers = {
+    	"content-type": "application/json",
+    	"X-RapidAPI-Key": spanki_key,
+    	"X-RapidAPI-Host": "rapid-translate-multi-traduction.p.rapidapi.com"
+    }
+    response = requests.post(url, json=payload, headers=headers, timeout=10)
+    if response.status_code != 200:
+        print(f'API {endpoint} returned {response.status_code}')
+        xlations[endpoint] =  None
+    else:
+        xlations[endpoint]= None if response.json()[0] == orig else response.json()[0]
+
+def long_translator_api(endpoint, text):
+    "Call the rapidapi long-translator call"
+    url = "https://long-translator.p.rapidapi.com/translate"
+
+    payload = {
+            "source_language": lang,
+            "target_language": "en",
+            "text": text
+            }
+    headers = {
 	        "content-type": "application/x-www-form-urlencoded",
-	        "X-RapidAPI-Key": os.environ.get('SPANKI_KEY'),
+	        "X-RapidAPI-Key": spanki_key,
 	        "X-RapidAPI-Host": "long-translator.p.rapidapi.com"
-        }
+    }
 
-        response = requests.post(url, data=payload, headers=headers, timeout=10)
+    response = requests.post(url, data=payload, headers=headers, timeout=10)
 
-        if response.status_code != 200:
-            print(f'API {endpoint} returned {response.status_code}')
-            xlations[endpoint] =  None
-        else:
-            xlations[endpoint] = None \
-                    if response.json()['data']['translatedText'].lower() == orig.lower() \
-                    else response.json()['data']['translatedText'].lower()
+    if response.status_code != 200:
+        print(f'API {endpoint} returned {response.status_code}')
+        xlations[endpoint] =  None
+    else:
+        xlations[endpoint] = None \
+                if response.json()['data']['translatedText'].lower() == orig.lower() \
+                else response.json()['data']['translatedText'].lower()
+
+def rest4dict_api(endpoint, text):
+    "call rest4dict running locally (http://gitub.com/kentonself/rest4dict)"
+    payload={'source': lang, 'target':'en', 'word': text}
+    response = requests.post('http://localhost:5000/api/dict', json=payload, timeout=10)
+    if response.status_code == 200 and response.json()['found'] == 1:
+        xlations[endpoint] = response.json()['xlation'].lower()
+    else:
+        xlations[endpoint] = None
+
 
 # check for env before continuing
 envset = True
-if os.environ.get('SPANKI_KEY') is None:
-    envset = False
-    print("Environment Variable SPANKI_KEY must be set to rapidapi key")
-    print()
+spanki_key =  os.environ.get('SPANKI_KEY')
+
 supported_apis_string = ','.join(supported_apis)
 if os.environ.get('SPANKI_APIS') is None:
     envset = False
@@ -91,11 +115,28 @@ else:
             envset = False
             print(f'{api} is not a supported api. Supported apis are {supported_apis_string}')
             print()
+        if api in rapidapi_apis and spanki_key is None:
+            envset = False
+            print(f"Environment Variable SPANKI_KEY must be set since {api} is a rapidapi service")
+            print()
+
+# allow for overwriting the default anki connect URL to run on remote instances of Anki/AnkiConnect
+ankiconnect_url = os.environ.get('SPANKI_ANKICONNECT_URL')
+if ankiconnect_url is None:
+    ankiconnect_url = 'http://localhost:8765'
+
+print(ankiconnect_url)
 
 # test for existance of Anki/AnkiConnect and get deckNames
-availdecks = requests.post(url = 'http://localhost:8765',\
-                           json={"action": "deckNames", "version": 6 }, \
-                           timeout=10)
+try:
+    availdecks = requests.post(url = ankiconnect_url,\
+                               json={"action": "deckNames", "version": 6 }, \
+                               timeout=10)
+except requests.ConnectionError:
+    envset = False
+    print( "Anki may not be running or AnkiConnect may not be installed")
+    print()
+
 if availdecks.json()['error']:
     envset = False
     print( "Anki may not be running or AnkiConnect may not be installed")
@@ -167,9 +208,12 @@ try:
         # (converting the python list to a python set removes dupliates)
         if found > 1 and len(set(choices)) > 1:
             while True:
+                concat = ' ,'.join(set(choices))
+                concat = re.sub('^ , ', ' ', concat, count=0, flags=0)
+
                 choice = input('Enter number choice from above or:\n' +\
                                 ' q to skip adding to anki\n' +\
-                                ' c to concatenate choices : ')
+                                ' c to concatenate choices\n   (' + concat + ') : ')
                 if choice.lower() in ['q', 'c']:
                     break
                 if not choice.isnumeric():
@@ -247,12 +291,12 @@ try:
                 }
 
             # Add the card
-            ankiresp = requests.post(url = 'http://localhost:8765', json=ankiact, timeout=10)
+            ankiresp = requests.post(url = ankiconnect_url, json=ankiact, timeout=10)
             print(ankiresp.json())
 
             # If sync is turned on, sync with AnkiWeb
             if sync_on_add.lower() not in  ['0', 'false']:
-                ankisync = requests.post(url = "http://localhost:8765", \
+                ankisync = requests.post(url = ankiconnect_url, \
                         json = { "action": "sync", "version": 6}, timeout=30)
                 print(ankisync.json())
 
